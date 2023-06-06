67ee836a286e8ce876a98b76ed24a85820d0e521
Nicolas G Munoz <nicolasgustavomunoz@gmail.com>
2023-06-06 17:09:20 -0300

    solucion boton borrar un solo item

--- js/carrito.js
+++ js/carrito.js
@@ -4,22 +4,47 @@ const finalizarCompra = document.getElementById("finalizarCompra")
 const carritoVacio = document.getElementById("carritoVacio")
 const loginVacio = document.getElementById("loginVacio")
 const borrarC = document.getElementById("borrarC")
+const borrarUnProducto = document.getElementsByClassName("borrarUnProducto")
+let carritoAct = []
+
+class Producto {
+    constructor (id, name, price, foto){
+        this.id = id
+        this.name = name
+        this.price = price
+        this.foto = foto
+    }
+}
+
+/* Recibo el local storage y creo un nuevo array */
+function estaEsLaVencida(){
+    let verCarrito = localStorage.getItem('carrito')
+    carrito = JSON.parse(verCarrito)
+    for (const producto of carrito){
+    carritoAct.push(new Producto(producto.id, producto.name, producto.price, producto.foto))
+    }
+}
+
+estaEsLaVencida()
 
 
-/*funcion que recibe el arrary del localstorage y setea informacion en el dom
+
+
+/*funcion que recibe el arrary creado en la funcion estaEsLaVencida() y setea informacion en el dom
 */
 function pasarCarrito (){
-    if(localStorage.getItem('carrito') !== null)
+    if(carritoAct.length > 0)
     {
         if (localStorage.getItem('cliente') !== null)
-        {
+        {   
             let verCliente = localStorage.getItem('cliente')
             cliente = JSON.parse(verCliente)
-            let verCarrito = localStorage.getItem('carrito')
-            carrito = JSON.parse(verCarrito)
-            carrito.forEach((producto)=>{
+            carritoAct.forEach((producto)=>{
                 let template = `
-                    <div class="col-lg-2"></div>          
+                    <div class="col-lg-1"></div>
+                    <div class="col-lg-1 bb bt ct">
+                    <button class="btn btn-primary transparent borrarUnProducto" value="${producto.id}">X</button>
+                    </div>            
                     <div class="col-lg-3 carritoClas ff bb">
                     <img src=".${producto.foto}" alt="" style="width: 8rem; height: 8rem;">
                     </div>
@@ -35,7 +60,7 @@ function pasarCarrito (){
             }) 
 
         
-            let montoFinal = carrito.reduce((i, producto) => {
+            let montoFinal = carritoAct.reduce((i, producto) => {
                 return i + producto.price
             }, 0)
             let template = `
@@ -63,11 +88,12 @@ function pasarCarrito (){
             
 
         }else{
-            let verCarrito = localStorage.getItem('carrito')
-            carrito = JSON.parse(verCarrito)
-            carrito.forEach((producto)=>{
+            carritoAct.forEach((producto)=>{
             let template = `
-            <div class="col-lg-2"></div>          
+            <div class="col-lg-1"></div>
+            <div class="col-lg-1 bb bt ct">
+            <button class="btn btn-primary transparent borrarUnProducto" value="${producto.id}">X</button>
+            </div>         
             <div class="col-lg-3 carritoClas ff bb">
             <img src=".${producto.foto}" alt="" style="width: 8rem; height: 8rem;">
             </div>
@@ -83,7 +109,7 @@ function pasarCarrito (){
             }) 
 
         
-            let montoFinal = carrito.reduce((i, producto) => {
+            let montoFinal = carritoAct.reduce((i, producto) => {
              return i + producto.price
             }, 0)
             let template = `
@@ -111,6 +137,9 @@ function pasarCarrito (){
 pasarCarrito()
 
 
+
+
+/* Boton borrar carrito completo */
 borrarC.addEventListener('click',() => {
     Toastify({
 
@@ -125,7 +154,7 @@ borrarC.addEventListener('click',() => {
        }, 2000);
 })
 
-
+/* Boton comprar carrito */
 comprar.addEventListener('click',() =>{
     Toastify({
 
@@ -139,3 +168,32 @@ comprar.addEventListener('click',() =>{
         location.reload()
        }, 2000);
 })
+
+/* Funcion que borra un solo producto */
+function borrarProdcuto () {
+    for (botonardo of borrarUnProducto){
+        let id = botonardo.value
+        botonardo.addEventListener('click', () =>{
+            const index2 = carritoAct.findIndex(producto => producto.id == id)
+            if (index2 !== -1){
+                carritoAct.splice(index2, 1)
+            }
+            localStorage.setItem('carrito', JSON.stringify(carritoAct))
+            
+
+            Toastify({
+
+                text: `Producto eliminado`,
+            
+                duration: 2000
+            
+            }).showToast();    
+            setTimeout(() => {
+                location.reload()
+               }, 100);
+
+        })
+    }
+}
+
+borrarProdcuto ()
--- js/index.js
+++ js/index.js
@@ -6,12 +6,7 @@ let min = 0;
 const btAddToCard = document.getElementsByClassName('btAddToCard')
 
 
-// const bt1 = document.getElementById("1")
-// const bt2 = document.getElementById("bt2")
-// const bt3 = document.getElementById("bt3")
-// const bt4 = document.getElementById("bt4")
-// const bt5 = document.getElementById("bt5")
-// const bt6 = document.getElementById("bt6")
+
 
 
 /*creamos los array´s de productos de la tienda*/
